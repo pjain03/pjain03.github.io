1d17d8a6a1895baa72a9addcbe6c77e6896dd5b2
Pulkit <Pulkit.Jain@tufts.edu>
2016-10-22 22:12:28 -0400

    positioning lines

--- script.js
+++ script.js
@@ -14,17 +14,16 @@ var lng;
 var image= './icon.png';
 var stationPosition= new Array(22);
 var stations= {
-    names: ["South Station", "Andrew", "Porter Square", "Harvard Square", "JFK/UMass", "Savin Hill", 
-    "Park Street", "Broadway", "North Quincy", "Shawmut", "Davis", "Alewife", "Kendall/MIT", 
-    "Charles/MGH", "Downtown Crossing", "Quincy Center", "Quincy Adams", "Ashmont", "Wollaston",
-    "Fields Corner", "Central Square", "Braintree"],
-    lat: [42.352271, 42.330154, 42.3884, 42.373362, 42.320685, 42.31129, 42.35639457, 42.342622,
-        42.275275, 42.29312583, 42.39674, 42.395428, 42.36249079, 42.361166, 42.355518, 42.251809,
-        42.233391, 42.284652, 42.2665139, 42.300093, 42.365486, 42.2078543],
-    lng: [-71.05524200000001, -71.057655, -71.11914899999999, -71.118956, -71.052391, -71.053331,
-        -71.0624242, -71.056967, -71.029583, -71.06573796000001, -71.121815, -71.142483, -71.08617653,
-        -71.070628, -71.060225, -71.005409, -71.007153, -71.06448899999999, -71.0203369, -71.061667, 
-        -71.103802, -71.0011385]
+    names: ["Alewife", "Davis", "Porter Square", "Harvard Square", "Central Square", "Kendall/MIT", 
+    "Charles/MGH", "Park Street", "Downtown Crossing", "South Station", "Broadway", "Andrew", "JFK/UMass", 
+    "North Quincy", "Wollaston", "Quincy Center", "Quincy Adams", "Braintree", "Savin Hill",
+    "Fields Corner", "Shawmut", "Ashmont"],
+    lat: [42.395428, 42.39674, 42.3884, 42.373362, 42.365486, 42.36249079, 42.361166, 42.35639457,
+    42.355518, 42.352271, 42.342622, 42.330154, 42.320685, 42.275275, 42.2665139, 42.251809, 42.233391,
+    42.2078543, 42.31129, 42.300093, 42.29312583, 42.284652],
+    lng: [-71.142483, -71.121815, -71.11914899999999, -71.118956, -71.103802, -71.08617653, -71.070628, 
+    -71.0624242, -71.060225, -71.05524200000001, -71.056967, -71.057655, -71.052391, -71.029583, -71.0203369,
+    -71.005409, -71.007153, -71.0011385, -71.053331, -71.061667, -71.06573796000001, -71.06448899999999]
 };
 var stationMarker;
 var stationInfoWindow;
